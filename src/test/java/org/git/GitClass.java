a71f09ea194e90848eea6753f6a87cf85aa0bf51
Aarthi617 <aarthisekaran3007@gmail.com>
2022-11-04 18:43:56 +0530

    Project structure

---
+++ src/test/java/org/git/GitClass.java
@@ -0,0 +1,21 @@
+package org.git;
+
+import org.testng.annotations.Test;
+
+public class GitClass {
+
+	@Test
+	private void testA1() {
+		System.out.println("testA1");
+	}
+
+	@Test
+	private void testA2() {
+		System.out.println("testA2");
+	}
+
+	@Test
+	private void testA3() {
+		System.out.println("testA3");
+	}
+}
